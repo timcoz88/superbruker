b608d76608c15e7b5e07c8249df9971aa6413d90
Bjørn Bakke <BjornBakke@live.com>
2018-06-18 19:07:17 +0200

    Fjernet Typografi konsept

--- src/SuperAdmin.tsx
+++ src/SuperAdmin.tsx
@@ -1,7 +1,6 @@
 import AppBar from '@material-ui/core/AppBar';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
-import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
 import { HashRouter, NavLink, Route } from "react-router-dom";
 import './App.css';
@@ -14,17 +13,10 @@ class SuperAdmin extends React.Component {
     value: 0,
   };
 
-  public TabContainer(props: any) {
-    return (
-      <Typography component="div" style={{ padding: 8 * 3 }}>
-        {props.children}
-      </Typography>
-    );
-  }
-
   public handleChange = (event: any, value: any) => {
     this.setState({ value });
   };
+
   public render() {
     const { value } = this.state;
     return (
@@ -36,18 +28,9 @@ class SuperAdmin extends React.Component {
               <Tab label="Brukeradmin" component={NavLink}{...{ to: '/brukeradmin' } as any} />
               <Tab label="Køadmin" component={NavLink}{...{ to: '/Køadmin' } as any} />
             </Tabs>
-
-            {value === 0 && <this.TabContainer>
-              <Route path="/" exact={true} component={Brevadmin} />
-            </this.TabContainer>}
-
-            {value === 1 && <this.TabContainer>
-              <Route path="/brukeradmin" component={Brukeradmin} />
-            </this.TabContainer>}
-
-            {value === 2 && <this.TabContainer>
-              <Route path="/Køadmin" component={Køadmin} />
-            </this.TabContainer>}
+            <Route path="/" exact={true} component={Brevadmin} />
+            <Route path="/brukeradmin" component={Brukeradmin} />
+            <Route path="/Køadmin" component={Køadmin} />
           </AppBar>
         </div>
       </HashRouter>
